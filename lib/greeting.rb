83af4874ac455d3a96dc1598cf8fb29881113fc9
Warda Ayaz <wayaz107@gmail.com>
2020-05-16 21:48:59 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 name = gets.strip
-def greeting(name = Floyd)
+def greeting(name)
   puts "Hi! I'm HAL, what's your name?"
   puts "Hello #{name}. It's nice to meet you."
 end
